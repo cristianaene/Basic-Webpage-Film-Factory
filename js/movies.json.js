29f98ce9babee5d01e4585ac0fca9184f1a98b48
Cristiana Ene <cristiana.mihaela.ene@gmail.com>
2021-07-02 20:29:45 +0200

    Adding beta

--- js/movies.json.js
+++ js/movies.json.js
@@ -1,73 +1,73 @@
 let data = `[
     {
         "title": "Arsenic and Old Lace",
-        "summary": "Arsenic and Old Lace is a 1944 American black comedy film directed by Frank Capra and starring Cary Grant. It was based on Joseph Kesselring's 1941 play, Arsenic and Old Lace. The script adaptation was written by Julius J. Epstein and Philip G. Epstein.[3] The contract with the play’s producers stipulated that the film would not be released until the Broadway run ended. The original planned release date was September 30, 1942. The play was a tremendous hit, running for three and a half years, so the film was not released until 1944. ",
+        "summary": "Arsenic and Old Lace is a 1944 American black comedy film directed by Frank Capra and starring Cary Grant",
         "img": "img/arsenic_and_old_lace.jpg",
         "likesNo": 5
     },
     {
         "title": "Casablanca",
-        "summary": "Casablanca is a 1942 American romantic drama film directed by Michael Curtiz, and starring Humphrey Bogart, Ingrid Bergman, and Paul Henreid. Filmed and set during World War II, it focuses on an American expatriate (Bogart) who must choose between his love for a woman (Bergman) or helping her and her husband (Henreid), a Czech resistance leader, escape from the Vichy-controlled city of Casablanca to continue his fight against the Germans. The screenplay is based on Everybody Comes to Rick's, an unproduced stage play by Murray Burnett and Joan Alison. The supporting cast features Claude Rains, Conrad Veidt, Sydney Greenstreet, Peter Lorre, and Dooley Wilson.",
+        "summary": "Casablanca is a 1942 American romantic drama film directed by Michael Curtiz, and starring Humphrey Bogart",
         "img": "img/casablanca.jpg",
         "likesNo": 12
     },
     {
         "title": "Breakfast at Tiffany's",
-        "summary": "Breakfast at Tiffany's is a 1961 American romantic comedy film directed by Blake Edwards, written by George Axelrod, adapted from Truman Capote's 1958 novella of the same name, and starring Audrey Hepburn as Holly Golightly, a naïve, eccentric café society girl who falls in love with a struggling writer (George Peppard). It also featured Patricia Neal, Buddy Ebsen, Martin Balsam, and Mickey Rooney in supporting roles.",
+        "summary": "Breakfast at Tiffany's is a 1961 American romantic comedy film directed by Blake Edwards, written by George Axelrod",
         "img": "img/breakfast.jpg",
         "likesNo": 15
     },
     {
         "title": "A Summer Place",
-        "summary": "A Summer Place is a 1959 American romantic drama film based on Sloan Wilson's 1958 novel of the same name, about teenage lovers from different social classes who get back together 20 years later, and then must deal with the passionate love affair of their own teenage children by previous marriages. Delmer Daves directed the movie, which stars Richard Egan and Dorothy McGuire as the middle-aged lovers, and Sandra Dee and Troy Donahue as their respective children. The film contains a memorable instrumental theme composed by Max Steiner, which spent nine weeks at number one on the Billboard Hot 100 singles chart in 1960.",
+        "summary": "A Summer Place is a 1959 American romantic drama film based on Sloan Wilson's 1958 novel of the same name",
         "img": "img/a_summer_place.jpg",
         "likesNo": 8
     },
     {
         "title": "Rebecca",
-        "summary": "Rebecca is a 1940 American romantic psychological thriller film directed by Alfred Hitchcock. It was Hitchcock's first American project, and his first film under contract with producer David O. Selznick. The screenplay by Robert E. Sherwood and Joan Harrison, and adaptation by Philip MacDonald and Michael Hogan, were based on the 1938 novel of the same name by Daphne du Maurier. The film stars Laurence Olivier as the brooding, aristocratic widower Maxim de Winter and Joan Fontaine as the young woman who becomes his second wife, with Judith Anderson, George Sanders and Gladys Cooper in supporting roles. The film is a gothic tale shot in black-and-white. Maxim de Winter's first wife Rebecca, who died before the events of the film, is never seen. Her reputation and recollections of her, however, are a constant presence in the lives of Maxim, his new wife and the housekeeper Mrs. Danvers.",
+        "summary": "Rebecca is a 1940 American romantic psychological thriller film directed by well known director Alfred Hitchcock",
         "img": "img/rebecca.jpg",
         "likesNo": 28
     },
     {
         "title": "Cat on a Hot Tin Roof",
-        "summary": "Cat on a Hot Tin Roof is a 1958 American drama film directed by Richard Brooks.[3][4] It is based on the 1955 Pulitzer Prize-winning play of the same name by Tennessee Williams and adapted by Richard Brooks and James Poe. The film stars Elizabeth Taylor, Paul Newman, Burl Ives, Judith Anderson, Jack Carson and Madeleine Sherwood.Well-received by both critics and audiences, Cat on a Hot Tin Roof was MGM's most successful release of 1958, and became the third highest-grossing film of that year.",
+        "summary": "Cat on a Hot Tin Roof is a 1958 American drama film directed by Richard Brooks. It is based on the play by Tennessee Williams",
         "img": "img/cat_on_a_hot_tin_roof.jpg",
         "likesNo": 30
     },
     {
         "title": "Citizen Kane",
-        "summary": "Citizen Kane is a 1941 American drama film produced and directed by Orson Welles, who also co-wrote the screenplay with Herman J. Mankiewicz. The picture was Welles's first feature film. Considered by many critics and filmmakers to be the greatest film ever made, Citizen Kane was voted number 1 in five consecutive British Film Institute Sight & Sound polls of critics, and it topped the American Film Institute's 100 Years ... 100 Movies list in 1998, as well as its 2007 update. Nominated for Academy Awards in nine categories, it won an Academy Award for Best Writing (Original Screenplay) by Mankiewicz and Welles. Citizen Kane is praised for Gregg Toland's cinematography, Robert Wise's editing, Bernard Herrmann's music, and its narrative structure, all of which have been considered innovative and precedent-setting.",
+        "summary": "Citizen Kane is a 1941 American drama film produced and directed by Orson Welles, who also co-wrote the screenplay with Herman J. Mankiewicz",
         "img": "img/citizen_kane.jpg",
         "likesNo": 41
     },
     {
         "title": "Rear Window",
-        "summary": "Rear Window is a 1954 American mystery thriller film directed by Alfred Hitchcock and written by John Michael Hayes based on Cornell Woolrich's 1942 short story It Had to Be Murder . Originally released by Paramount Pictures, the film stars James Stewart, Grace Kelly, Wendell Corey, Thelma Ritter, and Raymond Burr. It was screened at the 1954 Venice Film Festival.",
+        "summary": "Rear Window is a 1954 American mystery thriller film directed by Alfred Hitchcock and written by John Michael Hayes based on Cornel short story",
         "img": "img/rear_window.jpg",
         "likesNo": 25
     },
     {
         "title": "North by Northwest",
-        "summary": "North by Northwest is a 1959 American spy thriller film produced and directed by Alfred Hitchcock, starring Cary Grant, Eva Marie Saint, and James Mason. The screenplay was by Ernest Lehman, who wanted to write the Hitchcock picture to end all Hitchcock pictures .North by Northwest is a tale of mistaken identity, with an innocent man pursued across the United States by agents of a mysterious organization trying to prevent him from blocking their plan to smuggle out microfilm which contains government secrets. This is one of several Hitchcock films which feature a music score by Bernard Herrmann and an opening title sequence by graphic designer Saul Bass, and was the first to feature extended use of kinetic typography in its opening credits.",
+        "summary": "North by Northwest is a 1959 American spy thriller film produced and directed by Alfred Hitchcock, starring Cary Grant, Eva Marie Saint, and James Mason",
         "img": "img/north_by_northwest.jpg",
         "likesNo": 55
     },
     {
         "title": "Gone with the Wind",
-        "summary": "Gone with the Wind is a 1939 American epic historical romance film adapted from the 1936 novel by Margaret Mitchell. The film was produced by David O. Selznick of Selznick International Pictures and directed by Victor Fleming. Set in the American South against the backdrop of the American Civil War and the Reconstruction era, the film tells the story of Scarlett O'Hara, the strong-willed daughter of a Georgia plantation owner. It follows her romantic pursuit of Ashley Wilkes, who is married to his cousin, Melanie Hamilton, and her subsequent marriage to Rhett Butler. The leading roles are played by Vivien Leigh (Scarlett), Clark Gable (Rhett), Leslie Howard (Ashley), and Olivia de Havilland (Melanie).",
+        "summary": "Gone with the Wind is a 1939 American epic historical romance film adapted from the 1936 novel by Margaret Mitchell",
         "img": "img/gone_with_the_wind.jpg",
         "likesNo": 37
     },
     {
         "title": "The Birds",
-        "summary": "The Birds is a 1963 American natural horror-thriller film produced and directed by Alfred Hitchcock. Loosely based on the 1952 story of the same name by Daphne du Maurier, it focuses on a series of sudden and unexplained violent bird attacks on the people of Bodega Bay, California, over the course of a few days.",
+        "summary": "The Birds is a 1963 American natural horror-thriller film produced and directed by Alfred Hitchcock",
         "img": "img/the_birds.jpg",
         "likesNo": 47
     },
     {
         "title": "Some Like it Hot",
-        "summary": "Some Like It Hot is a 1959 American romantic comedy film directed, produced, and co-written by Billy Wilder. It stars Marilyn Monroe, Tony Curtis, Jack Lemmon, and features George Raft, Pat O'Brien, Joe E. Brown, Joan Shawlee, Grace Lee Whitney, and Nehemiah Persoff in supporting roles. The screenplay by Wilder and I. A. L. Diamond is based on a screenplay by Robert Thoeren and Michael Logan from the 1935 French film Fanfare of Love. The film is about two musicians who dress in drag in order to escape from mafia gangsters whom they witnessed committing a crime (inspired by the Saint Valentine's Day Massacre).",
+        "summary": "Some Like It Hot is a 1959 American romantic comedy film directed, produced, and co-written by Billy Wilder",
         "img": "img/some_like_it_hot.png",
         "likesNo": 19
     }
--- js/script.js
+++ js/script.js
@@ -1,34 +1,37 @@
 let movieList = JSON.parse(data);
-
-for (let i = 0; i < movieList.length; i++) {
-    let movieCard = `
-    <div class="col col-lg-4 col-md-6 col-sm-12">
-        <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="${movieList[i].img}" class="img-fluid rounded-start" alt="...">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">${movieList[i].title}</h5>
-                        <p class="card-text">${movieList[i].summary}.</p>
-                        <div class="d-grid gap-2 d-flex justify-content-end">
-                            <button class="btn bg-warning me-md-2 likebtn" type="button" name="${i}">
-                                Like 
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
-                                    <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
-                                </svg>
-                            </button>
-                            <span class="btn rounded-circle bg-warning likevalue" >${movieList[i].likesNo}</span>
+function addMovies(movies) {
+    for (let i = 0; i < movies.length; i++) {
+        let movieCard = `
+        <div class="col col-lg-4 col-md-6 col-sm-12 col-12">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col col-lg-4 col-md-4 col-sm-12 col-12">
+                        <img src="${movies[i].img}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col col-lg-8 col-md-8 col-sm-12 col-12">
+                        <div class="card-body">
+                            <h5 class="card-title">${movies[i].title}</h5>
+                            <p class="card-text">${movies[i].summary}.</p>
+                            <div class="d-grid gap-2 d-flex justify-content-end">
+                                <button class="btn bg-warning likebtn" type="button" name="${i}">
+                                    Like 
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">
+                                        <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>
+                                    </svg>
+                                </button>
+                                <span class="btn rounded-circle bg-warning likevalue" >${movies[i].likesNo}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    `;
-    document.getElementById("moviecontainer").innerHTML += movieCard;
+        `;
+        document.getElementById("moviecontainer").innerHTML += movieCard;
+    }
+    addNewLikes();
 }
+addMovies(movieList);
 
 // defining function to increment the number of likes
 function incrementLikes() {
@@ -39,11 +42,23 @@ function incrementLikes() {
    
 }
 
-
-
-// add event listener to all like buttons
-let likeButtons = document.getElementsByClassName("likebtn");
-for (let i = 0; i <likeButtons.length; i++) {
-    likeButtons[i].addEventListener("click", incrementLikes);
+// add event listener to all like buttons and include them in a function to recalculate likes after sorting
+function addNewLikes () {
+    let likeButtons = document.getElementsByClassName("likebtn");
+    for (let i = 0; i <likeButtons.length; i++) {
+        likeButtons[i].addEventListener("click", incrementLikes);
+    }
 }
+
+// sort films by number of likes
+function sortLikes () {
+    movieList.sort((a, b) => {
+        return b.likesNo - a.likesNo;
+    });
+
+    // empty html movielist container
+    document.getElementById("moviecontainer").innerHTML = "";
+    addMovies(movieList);
+}
+document.getElementById("likesort").addEventListener("click", sortLikes);
 
